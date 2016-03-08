b3ec336c3050e9377a61c898cebc6c8805cd4b85
zhenglong <tomorrow_is_good@hotmail.com>
2016-03-08 20:05:29 +0800

    add agent_detail.html

---
+++ src/modules/agent_detail.js
@@ -0,0 +1,70 @@
+import Settings from '../components/settings';
+import StyleIntro from '../styles/intro.less';
+import styleApp from '../styles/app.scss';
+import Accordion from '../components/accordion.js';
+import {Tabs, TabPanel} from '../components/tabs.js';
+import Button from '../components/button.js';
+import styleContentBlock from '../components/content-block.less';
+import styleIndex from '../styles/index.scss';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import {Dom7 as $} from '../components/dom7.js';
+import SalesUtil from '../components/sales-util.js';
+
+Accordion.registerEvents();
+Tabs.registerEvents();
+
+SalesUtil.get('/api/sales/agent/info/', function(result) {
+    var detail = result.data;
+    detail.agentName = '张三儿'; // TODO: fetch from url
+    ReactDOM.render(
+        <div className="container-placeholder">
+            <div className='statusbar-overlay'></div>
+            <div className="views">
+                <div id="view-1" className="view view-main">
+                    <div className="pages">
+                        <div data-page="index-1" className="page ">
+                            <div className="page-content">
+                                <div className="info">
+                                    <div>
+                                        <img src={detail.headUrl || "img/placeholder.png"} className="photo" />
+                                        <p>
+                                            {detail.salesArea}<br />
+                                            所属{detail.managerRole} {detail.managerName}
+                                        </p>
+                                    </div>
+                                    <span>
+                                        {detail.agentName}
+                                    </span>
+                                </div>
+                                <div className="content-block-title">
+                                    下属{detail.managedRole}
+                                </div>
+                                <div className="list-block">
+                                    <ul>
+                                    {
+                                        (detail.managedAreas || []).map(function(item) {
+                                            return <li className="item-content">
+                                                <div className="item-inner">
+                                                    <div className="item-title">
+                                                        <img src={item.headUrl || "img/placeholder.png"} className="photo" />
+                                                        <span>{item.agentName}</span>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        });
+                                    }
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>, document.getElementsByClassName('body')[0]);
+        window.setTimeout(function() {
+            if (list.length == 1) {
+                Accordion.open($('.accordion-item')[0]);
+            }
+        }, 0);
+});
